436cdef26f4209312929617e38dd9627328907f2
lizeyang1995 <424011527@qq.com>
2020-09-23 17:44:39 +0800

    [lizeyang] faet: add Submit component

--- src/components/MyProfile.js
+++ src/components/MyProfile.js
@@ -4,26 +4,35 @@ import Name from './Name';
 import Gender from './Gender';
 import Description from './Description';
 import Conduct from './Conduct';
+import Submit from './Submit';
 
 class MyProfile extends Component {
   state = {
     name: '',
-    gender: '',
+    gender: 'Male',
     description: ''
   }
+
   handleChange = (event) => {
     this.setState({
       [event.target.name]: event.target.value
     })
   }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(JSON.stringify(this.state));
+  }
+  
   render() {
     return (
-      <form>
+      <form onSubmit={this.handleSubmit}>
         <h1>My Profile</h1>
         <Name onChange={this.handleChange} value={this.state.name} />
         <Gender onChange={this.handleChange} value={this.state.gender} />
         <Description onChange={this.handleChange} value={this.state.description} />
         <Conduct />
+        <Submit />
       </form>
     );
   }
---
+++ src/components/Submit.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+
+class Submit extends Component {
+  render() {
+    return (
+      <input 
+        type='submit'
+        name='name'
+        value='Submit'
+      />
+    );
+  }
+}
+export default Submit;
