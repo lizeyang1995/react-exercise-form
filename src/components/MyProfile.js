7c7ffedaac66a115c09ef0f61f6c26b0e27e7f75
lizeyang1995 <424011527@qq.com>
2020-09-23 16:20:11 +0800

    [lizeyang] faet: add Name component

--- src/components/MyProfile.js
+++ src/components/MyProfile.js
@@ -1,10 +1,22 @@
 import React, {Component} from 'react';
 import './myProfile.less';
+import Name from './Name';
 
 class MyProfile extends Component {
+  state = {
+    name: ''
+  }
+  handleChange = (event) => {
+    this.setState({
+      name: event.target.value
+    })
+  }
   render() {
     return (
-      <form></form>
+      <form>
+        <h1>My Profile</h1>
+        <Name onChange={this.handleChange} value={this.state.name} />
+      </form>
     );
   }
 }
---
+++ src/components/Name.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+
+class Name extends Component {
+  render() {
+    return (
+      <label>
+        <h2>Name</h2>
+        <input 
+          type='text' 
+          name='name' 
+          onChange={this.props.onChange}
+          value={this.props.value}
+        />
+      </label>
+    );
+  }
+}
+
+export default Name;
+
